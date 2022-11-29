0ac144f7ad88ebac4f5968547267449c08212cba
ArtemyevArtyom <gof.ru@yandex.ru>
2022-11-29 10:12:12 +0400

    Создание второго метода

--- Program.cs
+++ Program.cs
@@ -18,3 +18,11 @@ void ArrayConversion(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
