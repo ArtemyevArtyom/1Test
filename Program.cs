cdb1ad4f6037afd94e32cad303ef8ed716020f14
ArtemyevArtyom <gof.ru@yandex.ru>
2022-11-29 12:28:45 +0400

    добавление блок-схемы

--- Program.cs
+++ Program.cs
@@ -4,17 +4,17 @@
 либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 лучше обойтись исключительно массивами
 */
-string[] array1 = new string[4] {"hello", "2", "world", ":-)"};
+string[] array1 = new string[4] { "hello", "2", "world", ":-)" };
 string[] array2 = new string[array1.Length];
 void ArrayConversion(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+            array2[count] = array1[i];
+            count++;
         }
     }
 }
